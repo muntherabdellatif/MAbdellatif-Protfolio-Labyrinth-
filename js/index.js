d526eceb745afe6dd70286beeee6de0f79a4f23e
DESKTOP-8F9PJFG\AL-JWHARA <monthersaleh366@gmail.com>
2022-02-15 08:55:46 +0200

    adding more animations

--- js/index.js
+++ js/index.js
@@ -5,68 +5,76 @@ const downLink = document.getElementById("down");
 const leftLink = document.getElementById("left");
 const rightLink = document.getElementById("right");
 const otherSideLink = document.getElementById("other-side");
+const navBar = document.querySelectorAll("nav ul li");
 
 const sectionsPosition = {
-  current : 0,
-  otherSide : 5,
-  left : 1,
-  right : 4,
-  top : 3,
-  bottom: 2 
-}
+  current: 0,
+  otherSide: 5,
+  left: 1,
+  right: 4,
+  top: 3,
+  bottom: 2,
+};
+
+const sectionsNames = [
+  "section1",
+  "sections2",
+  "section3",
+  "section4",
+  "section5",
+  "section6",
+];
 
 let pageDirection = true;
 
-// scroll dirction functions 
+// scroll dirction functions
 
-function scrollVertically () {
+function scrollVertically() {
   container.classList.add("container-scroll-vertically");
   container.classList.remove("container-scroll-horizontally");
 }
 
-function scrollHorizontally () {
+function scrollHorizontally() {
   container.classList.add("container-scroll-horizontally");
   container.classList.remove("container-scroll-vertically");
 }
 
-function reverseDirection () {
+function reverseDirection() {
   container.classList.add("change-direction");
   pageDirection = false;
 }
 
-function rightDirection () {
+function rightDirection() {
   container.classList.remove("change-direction");
   pageDirection = true;
 }
 
-function changeDirection () {
+function changeDirection() {
   if (pageDirection) {
     reverseDirection();
-  }else {
+  } else {
     rightDirection();
   }
 }
 
 // control sections functions
 
-function goToSection (sectionNumber) {
-  sections[sectionNumber].scrollIntoView(
-    {
+function goToSection(sectionNumber) {
+  sections[sectionNumber].scrollIntoView({
     behavior: "smooth",
     block: "end",
-    inline: "nearest"
-  }
-  );
+    inline: "nearest",
+  });
 }
 
 function showCurrent() {
-  sections.forEach((section,index)=>{
-    if (index!==sectionsPosition.current){
+  sections.forEach((section, index) => {
+    if (index !== sectionsPosition.current) {
       section.classList.add("hidden");
-    }else {
+    } else {
       section.classList.remove("hidden");
     }
-  })
+  });
 }
 
 function showSection(sectionNumber) {
@@ -77,99 +85,136 @@ function showSection(sectionNumber) {
 // moving Function
 function goDown() {
   scrollVertically();
-  if (sectionsPosition.current>sectionsPosition.bottom){
+  if (sectionsPosition.current > sectionsPosition.bottom) {
     reverseDirection();
-  }else {
+  } else {
     rightDirection();
   }
   showSection(sectionsPosition.bottom);
   goToSection(sectionsPosition.bottom);
-  setTimeout(() => {  
+  setTimeout(() => {
     sectionsPosition.top = sectionsPosition.current;
     sectionsPosition.current = sectionsPosition.bottom;
     sectionsPosition.bottom = sectionsPosition.otherSide;
-    sectionsPosition.otherSide =5 - sectionsPosition.current;
+    sectionsPosition.otherSide = 5 - sectionsPosition.current;
     showCurrent(sectionsPosition.current);
-  }, 500);
+  }, 400);
+  changeNavName();
 }
 
 function goUp() {
   scrollVertically();
-  if (sectionsPosition.current<sectionsPosition.top){
+  if (sectionsPosition.current < sectionsPosition.top) {
     reverseDirection();
-  }else {
+  } else {
     rightDirection();
   }
   showSection(sectionsPosition.top);
   goToSection(sectionsPosition.top);
-  setTimeout(() => {  
+  setTimeout(() => {
     sectionsPosition.bottom = sectionsPosition.current;
     sectionsPosition.current = sectionsPosition.top;
     sectionsPosition.top = sectionsPosition.otherSide;
-    sectionsPosition.otherSide =5 - sectionsPosition.current;
+    sectionsPosition.otherSide = 5 - sectionsPosition.current;
     showCurrent(sectionsPosition.current);
-  }, 500);
+  }, 400);
+  changeNavName();
 }
 
 function goLeft() {
   scrollHorizontally();
-  if (sectionsPosition.current<sectionsPosition.left){
+  if (sectionsPosition.current < sectionsPosition.left) {
     reverseDirection();
-  }else {
+  } else {
     rightDirection();
   }
   showSection(sectionsPosition.left);
   goToSection(sectionsPosition.left);
-  setTimeout(() => {  
+  setTimeout(() => {
     sectionsPosition.right = sectionsPosition.current;
     sectionsPosition.current = sectionsPosition.left;
     sectionsPosition.left = sectionsPosition.otherSide;
-    sectionsPosition.otherSide =5 - sectionsPosition.current;
+    sectionsPosition.otherSide = 5 - sectionsPosition.current;
     showCurrent(sectionsPosition.current);
-  }, 500);
+  }, 400);
+  changeNavName();
 }
 
 function goRight() {
   scrollHorizontally();
-  if (sectionsPosition.current>sectionsPosition.right){
+  if (sectionsPosition.current > sectionsPosition.right) {
     reverseDirection();
-  }else {
+  } else {
     rightDirection();
   }
   showSection(sectionsPosition.right);
   goToSection(sectionsPosition.right);
-  setTimeout(() => {  
+  setTimeout(() => {
     sectionsPosition.left = sectionsPosition.current;
     sectionsPosition.current = sectionsPosition.right;
     sectionsPosition.right = sectionsPosition.otherSide;
-    sectionsPosition.otherSide =5 - sectionsPosition.current;
+    sectionsPosition.otherSide = 5 - sectionsPosition.current;
     showCurrent(sectionsPosition.current);
-  }, 500);
+  }, 400);
+  changeNavName();
 }
 
 function goOtherSide() {
-  sections[sectionsPosition.current].classList.add("front-section");
-  setTimeout(() => {  
-    sectionsPosition.current = sectionsPosition.otherSide;
-    sectionsPosition.otherSide =5 - sectionsPosition.current;
-    showCurrent(sectionsPosition.current);
-    sections[sectionsPosition.current].classList.remove("front-section");
-  }, 1000);
+  if (pageDirection) {
+    goRight();
+    setTimeout(() => {
+      goUp();
+      setTimeout(() => {
+        goUp();
+        setTimeout(() => {
+          goUp();
+          setTimeout(() => {
+            goRight();
+            pageDirection = false;
+          }, 400);
+        }, 400);
+      }, 400);
+    }, 400);
+  } else {
+    goDown();
+    setTimeout(() => {
+      goLeft();
+      setTimeout(() => {
+        goLeft();
+        setTimeout(() => {
+          goLeft();
+          setTimeout(() => {
+            goDown();
+            pageDirection = true;
+          }, 400);
+        }, 400);
+      }, 400);
+    }, 400);
+  }
 }
 
-// moving event listener 
-downLink.addEventListener("click",goDown);
-UpLink.addEventListener("click",goUp);
-leftLink.addEventListener("click",goLeft);
-rightLink.addEventListener("click",goRight);
-otherSideLink.addEventListener("click",goOtherSide);
+// nav animation
+
+function changeNavName() {
+  navBar.forEach((e) => e.classList.add("color"));
+  setTimeout(() => {
+    navBar.forEach((e) => e.classList.remove("color"));
+    UpLink.textContent = sectionsNames[sectionsPosition.top];
+    downLink.textContent = sectionsNames[sectionsPosition.bottom];
+    leftLink.textContent = sectionsNames[sectionsPosition.left];
+    rightLink.textContent = sectionsNames[sectionsPosition.right];
+    otherSideLink.textContent = sectionsNames[sectionsPosition.otherSide];
+  }, 1500);
+}
+
+// moving event listener
+downLink.addEventListener("click", goDown);
+UpLink.addEventListener("click", goUp);
+leftLink.addEventListener("click", goLeft);
+rightLink.addEventListener("click", goRight);
+otherSideLink.addEventListener("click", goOtherSide);
 
 // on load
 goToSection(sectionsPosition.current);
 showCurrent();
-
-// function scrollHandler () {
-
-// }
-
-// container.addEventListener("scroll", scrollHandler);
+changeNavName();
