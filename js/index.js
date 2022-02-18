2e8828832a9daa936c8cb7ae4aae2a9e2157cece
DESKTOP-8F9PJFG\AL-JWHARA <monthersaleh366@gmail.com>
2022-02-18 07:43:35 +0200

    improving scrolling behavior

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const sections = document.querySelectorAll(".section");
-const container = document.querySelector(".container");
-const UpLink = document.getElementById("up");
-const downLink = document.getElementById("down");
-const leftLink = document.getElementById("left");
-const rightLink = document.getElementById("right");
-const otherSideLink = document.getElementById("other-side");
-const navBar = document.querySelectorAll("nav ul li");
+const sections = document.querySelectorAll('.section');
+const container = document.querySelector('.container');
+const UpLink = document.getElementById('up');
+const downLink = document.getElementById('down');
+const leftLink = document.getElementById('left');
+const rightLink = document.getElementById('right');
+const otherSideLink = document.getElementById('other-side');
+const navBar = document.querySelectorAll('nav ul li');
 
 const sectionsPosition = {
   current: 0,
@@ -17,36 +17,42 @@ const sectionsPosition = {
 };
 
 const sectionsNames = [
-  "section1",
-  "sections2",
-  "section3",
-  "section4",
-  "section5",
-  "section6",
+  'section0',
+  'section1',
+  'section2',
+  'section3',
+  'section4',
+  'section5',
 ];
 
+let delayTime = 600;
+let isScrolling = false;
 let pageDirection = true;
-let nextSectionPosition = "";
+let nextSectionPosition = '';
 // scroll direction functions
 
 function scrollVertically() {
-  container.classList.add("container-scroll-vertically");
-  container.classList.remove("container-scroll-horizontally");
+  container.classList.add('container-scroll-vertically');
+  container.classList.remove('container-scroll-horizontally');
+  console.log('vertically');
 }
 
 function scrollHorizontally() {
-  container.classList.add("container-scroll-horizontally");
-  container.classList.remove("container-scroll-vertically");
+  container.classList.add('container-scroll-horizontally');
+  container.classList.remove('container-scroll-vertically');
+  console.log('horizontally');
 }
 
 function reverseDirection() {
-  container.classList.add("change-direction");
+  container.classList.add('change-direction');
   pageDirection = false;
+  console.log('reverse');
 }
 
 function rightDirection() {
-  container.classList.remove("change-direction");
+  container.classList.remove('change-direction');
   pageDirection = true;
+  console.log('right');
 }
 
 function changeDirection() {
@@ -61,53 +67,74 @@ function changeDirection() {
 
 function goToSection(sectionNumber) {
   sections[sectionNumber].scrollIntoView({
-    behavior: "smooth",
-    block: "end",
-    inline: "nearest",
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
   });
 }
 
+const goToSectionPro = (sectionNumber) => {
+  return new Promise((resolve, reject) => {
+    goToSection(sectionNumber);
+    setTimeout(() => {
+      resolve(true);
+    }, delayTime);
+  });
+};
+
 function showCurrent() {
+  container.classList.add('no-scroll');
   sections.forEach((section, index) => {
     if (index !== sectionsPosition.current) {
-      section.classList.add("hidden");
+      section.classList.add('hidden');
     } else {
-      section.classList.remove("hidden");
+      section.classList.remove('hidden');
     }
   });
 }
 
 function showSection(sectionNumber) {
+  container.classList.remove('no-scroll');
   console.log(sectionNumber);
-  sections[sectionNumber].classList.remove("hidden");
+  sections[sectionNumber].classList.remove('hidden');
 }
+
 // moving Function
 
 function findOppositeSide(direction) {
-  const side = ["bottom", "top", "left", "right"];
-  const opposite = ["top", "bottom", "right", "left"];
+  const side = ['bottom', 'top', 'left', 'right'];
+  const opposite = ['top', 'bottom', 'right', 'left'];
   let oppositeSide;
-  side.forEach((s, i) => {
-    if (direction === side) {
-      oppositeSide = opposite[i];
+  for (let s = 0; s < side.length; s++) {
+    if (direction === side[s]) {
+      oppositeSide = opposite[s];
       return oppositeSide;
     }
-  });
+  }
 }
 
+const setScrollDirectionPro = (direction) => {
+  return new Promise((resolve, reject) => {
+    setScrollDirection(direction);
+    setTimeout(() => {
+      resolve(true);
+    }, 100);
+  });
+};
+
 function setScrollDirection(direction) {
-  if (direction === "bottom" || direction === "top") {
+  if (direction === 'bottom' || direction === 'top') {
     scrollVertically();
-  } else if (direction === "left" || direction === "right") {
+  } else if (direction === 'left' || direction === 'right') {
     scrollHorizontally();
   }
-  if (direction === "bottom" || direction === "right") {
+  if (direction === 'bottom' || direction === 'right') {
     if (sectionsPosition.current > sectionsPosition[direction]) {
       reverseDirection();
     } else {
       rightDirection();
     }
-  } else if (direction === "left" || direction === "top") {
+  } else if (direction === 'left' || direction === 'top') {
     if (sectionsPosition.current < sectionsPosition[direction]) {
       reverseDirection();
     } else {
@@ -117,66 +144,66 @@ function setScrollDirection(direction) {
 }
 
 function reorderingSections(direction, oppositeSide) {
+  console.log(direction, oppositeSide);
   sectionsPosition[oppositeSide] = sectionsPosition.current;
   sectionsPosition.current = sectionsPosition[direction];
   sectionsPosition[direction] = sectionsPosition.otherSide;
   sectionsPosition.otherSide = 5 - sectionsPosition.current;
+  console.log(sectionsPosition);
 }
 
-function go(direction) {
-  nextSectionPosition=direction;
-  oppositeSide = findOppositeSide(direction);
-  setScrollDirection(direction);
+async function go(direction) {
+  nextSectionPosition = direction;
+  await setScrollDirectionPro(direction);
   showSection(sectionsPosition[direction]);
-  goToSection(sectionsPosition[direction]);
-  setTimeout(() => {
-    reorderingSections(direction, oppositeSide);
-    showCurrent(sectionsPosition.current);
-  }, 400);
+  await goToSectionPro(sectionsPosition[direction]);
+  oppositeSide = findOppositeSide(direction);
+  reorderingSections(direction, oppositeSide);
+  showCurrent(sectionsPosition.current);
 }
 
 function goOtherSide() {
   if (pageDirection) {
-    go("right");
+    go('right');
     setTimeout(() => {
-      go("top");
+      go('top');
       setTimeout(() => {
-        go("top");
+        go('top');
         setTimeout(() => {
-          go("top");
+          go('top');
           setTimeout(() => {
-            go("right");
+            go('right');
             changeNavName();
             pageDirection = false;
-          }, 400);
-        }, 400);
-      }, 400);
-    }, 400);
+          }, delayTime + 200);
+        }, delayTime + 200);
+      }, delayTime + 200);
+    }, delayTime + 200);
   } else {
-    go("bottom");
+    go('bottom');
     setTimeout(() => {
-      go("left");
+      go('left');
       setTimeout(() => {
-        go("left");
+        go('left');
         setTimeout(() => {
-          go("left");
+          go('left');
           setTimeout(() => {
-            go("bottom");
+            go('bottom');
             changeNavName();
             pageDirection = true;
-          }, 400);
-        }, 400);
-      }, 400);
-    }, 400);
+          }, delayTime + 200);
+        }, delayTime + 200);
+      }, delayTime + 200);
+    }, delayTime + 200);
   }
 }
 
 // nav animation
 
 function changeNavName() {
-  navBar.forEach((e) => e.classList.add("color"));
+  navBar.forEach((e) => e.classList.add('color'));
   setTimeout(() => {
-    navBar.forEach((e) => e.classList.remove("color"));
+    navBar.forEach((e) => e.classList.remove('color'));
     UpLink.textContent = sectionsNames[sectionsPosition.top];
     downLink.textContent = sectionsNames[sectionsPosition.bottom];
     leftLink.textContent = sectionsNames[sectionsPosition.left];
@@ -186,27 +213,43 @@ function changeNavName() {
 }
 
 // moving event listener
-downLink.addEventListener("click", () => {
-  go("bottom");
+downLink.addEventListener('click', () => {
+  go('bottom');
   changeNavName();
 });
-UpLink.addEventListener("click", () => {
-  go("top");
+
+UpLink.addEventListener('click', () => {
+  go('top');
   changeNavName();
 });
-leftLink.addEventListener("click", () => {
-  go("left");
+
+leftLink.addEventListener('click', () => {
+  go('left');
   changeNavName();
 });
-rightLink.addEventListener("click", () => {
-  go("right");
+
+rightLink.addEventListener('click', () => {
+  go('right');
   changeNavName();
 });
-otherSideLink.addEventListener("click", () => {
+
+otherSideLink.addEventListener('click', () => {
   goOtherSide();
 });
 
 // on load
-goToSection(sectionsPosition.current);
+goToSectionPro(sectionsPosition.current);
 showCurrent();
 changeNavName();
+
+container.onscroll = function () {
+  let nextSection = sections[sectionsPosition[nextSectionPosition]];
+  if (
+    Math.abs(container.scrollTop) + 200 >= Math.abs(container.clientHeight) ||
+    Math.abs(container.scrollLeft) + 100 >= Math.abs(container.clientHeight)
+  ) {
+    isScrolling = true;
+  } else {
+    isScrolling = false;
+  }
+};
